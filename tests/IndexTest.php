fb8ddab9b956d6832e7a587cd7f264f4578d7e04
sinmokkk <ncyu1112941@gmail.com>
2025-05-29 22:20:51 +0800

    Test 1

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -10,13 +10,11 @@ final class IndexTest extends TestCase
         $_POST['username'] = 'admin';
         $_POST['password'] = '1234';
 
-        // 建立測試資料庫
         $db = new SQLite3('users.db');
         $db->exec("DROP TABLE IF EXISTS users");
         $db->exec("CREATE TABLE users (username TEXT, password TEXT)");
         $db->exec("INSERT INTO users (username, password) VALUES ('admin', '1234')");
 
-        // 開始捕捉輸出
         ob_start();
         include __DIR__ . '/../index.php';
         $output = ob_get_clean();
